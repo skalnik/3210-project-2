880624aef8022dc6ddd460c338b0c8d124c0751c
Nicholas Smith <nix@nix-laptop.(none)>
2012-03-11 18:08:25 -0400

    basic kernel, sans kill. need to change to linked list

--- buzzLockK.c
+++ buzzLockK.c
@@ -3,34 +3,60 @@
 
 #include "buzzLockU.h"
 
-extern bzz_t;
+extern struct bzz_t;
 extern useconds_t;
-extern bzz_thread_t;
+extern struct bzz_thread_t;
+extern BZZ_BLACK;
+extern BZZ_GOLD;
 
 
-
-init_bzz( bzz_t *lock, int num_threads, useconds_t timeout){
-	&lock -> max_num_threads = num_threads;
-	&lock -> timeout = timeout;
-	*(&lock ->threads) = bzz_thread_t[num_threads];
+init_bzz(struct bzz_t *lock, int num_threads, useconds_t timeout){
+	lock -> max_num_threads = num_threads;
+	lock -> timeout = timeout;
+	*(lock ->threads) = bzz_thread_t[num_threads];
 }
 /* the current thread has color black/gold with respect to lock lock */
 
 bzz_color(int color, bzz_t *lock){
+	spin_lock_irqsave(&lock->mutex, lock->flags);
 
+	int i = 0;
+	struct bzz_thread_t *temp;
+	/*iterate through lock's thread list, find the calling thread's entry, set the color*/
+	for(;i<&lock->max_num_threads;i++){
+		*temp = &lock -> threads[i];
+		if (temp->TID == gettid()){
+			temp->BZZ_COLOR = color;
+		}
+	}
 
+	spin_lock_irqrestore(&lock->mutex, lock->flags);
 }
 
 /* Acquire the lock */
 
-bzz_lock( bzz_t lock){
+bzz_lock(struct bzz_t *lock){
+	spin_lock_irqsave(&lock->mutex, lock->flags);
+
+	if (lock -> locked == 0){
+		lock -> locked = 1;
+		lock -> locking_thread_PID = gettid();
+	}
+
+	spin_lock_irqrestore(&lock->mutex, lock->flags);
 
 }
-
 /* Release the lock */
 
-bzz_release (bzz_t lock){
+bzz_release (struct bzz_t *lock){
+	spin_lock_irqsave(&lock->mutex, lock->flags);
 
+	if (lock -> locked == 1){
+		if(lock -> locking_thread_PID == gettid()){
+			lock -> locked = 0;
+		}
+	}
+	spin_lock_irqrestore(&lock->mutex, lock->flags);
 }
 
 /* Destroy the lock */
--- buzzLockK.h
+++ buzzLockK.h
@@ -1,4 +1,5 @@
-#include <pthreads.h>
+#include <linux/types.h>
+#include <linux/spinlock.h>
 
 #ifndef BUZZLOCKU_H_
 #define BUZZLOCKU_H_
@@ -8,12 +9,15 @@ typedef int BZZ_BLACK = 1;
 typedef int BZZ_GOLD = 0;
 
 typedef struct{
-	pthread_t thread;
+	tid_t TID;
 	int BZZ_COLOR = -1;
 }bzz_thread_t;
 
 typedef struct {
+	spinlock_t mutex = SPIN_LOCK_UNLOCKED;
+	unsigned long flags;
 	int locked = 0;
+	int locking_thread_PID;
 	int max_num_threads = 0;
 	useconds_t timeout = 0;
 	bzz_thread_t *threads;

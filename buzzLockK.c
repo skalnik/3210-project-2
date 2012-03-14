d8e5f303700491ec64ca74b6550562538e1c27ae
Nicholas Smith <nix@nix-laptop.(none)>
2012-03-14 07:11:40 -0400

    bzz_kill & bzz_release not done, bzz_lock quarter done

--- buzzLockK.c
+++ buzzLockK.c
@@ -2,14 +2,19 @@
  * with a gold priority elevation timeout period of timeout usecs.*/
 #include "buzzLockK.h"
 #include <linux/slab.h>
-
-extern GFP_ATOMIC;
+extern GFP_NOFAIL;
 
 
 init_bzz(bzz_t *lock, int num_threads, suseconds_t timeout){
-	lock -> num_locked = num_threads;
 	lock -> timeout = timeout;
+	lock ->num_active = num_threads;
+	lock ->black_num = 0;
+	lock ->gold_num = 0;
+	lock ->old_gold_num = 0;
 	INIT_LIST_HEAD(&lock -> known_threads.list);
+	DECLARE_WAIT_QUEUE_HEAD(&lock->gold_queue);
+	DECLARE_WAIT_QUEUE_HEAD(&lock->black_queue);
+	DECLARE_WAIT_QUEUE_HEAD(&lock->old_gold_queue);
 }
 
 /* the current thread has color black/gold with respect to lock lock */
@@ -21,59 +26,112 @@ bzz_color(int color, bzz_t *lock){
 	/*iterate through lock's thread list, find the calling thread's entry, set the color*/
 
 	list_for_each(pos, &lock->known_threads.list){
-		temp = list_entry(pos,&lock->known_threads.list);
+		temp = list_entry(pos,&lock->known_threads.list, list);
 
 		if (temp->tid == current->tgid){
+			tmp = temp;
 			temp->BZZ_COLOR = color;
 			break;
 		}
-		else{/*thread doesnt have an entry, so we create and add it*/
-			tmp = (struct bzz_thread_t *) kmalloc(sizeof(bzz_thread_t), GFP_ATOMIC);
-			if (tmp){
-				spin_lock_irqsave(&lock->mutex, lock->flags);
 
-				list_add(&tmp->list, &lock->known_threads.list);
+	}
+	if (!tmp){/*thread doesnt have an entry, so we create and add it*/
+		tmp = (struct bzz_thread_t *) kmalloc(sizeof(bzz_thread_t), GFP_NOFAIL);
+		if (tmp){
+			tmp ->BZZ_COLOR = color;
+			tmp ->tid = current ->tgid;
+			tmp ->queue_time = (current_kernel_time().tv_nsec)*1000; /*convert nano to micro seconds*/
+			spin_lock_irqsave(&lock->mutex, lock->flags);
 
-				spin_lock_irqrestore(&lock->mutex, lock->flags);
-			}
+			list_add_tail(&tmp->list, &lock->known_threads.list);
+
+			spin_lock_irqrestore(&lock->mutex, lock->flags);
 		}
+	}
 	}
 }
 
 /* Acquire the lock */
 
 bzz_lock(bzz_t *lock){
-	spin_lock_irqsave(&lock->mutex, lock->flags);
 	/* check active #
 	 * check if gold has been waiting too long
+	 * if no black, queue gold
 	 * queue black
-	 * if available queue gold
 	 * */
-	if (lock->num_locked == 0){ /*max active threads*/
+	 /*max active threads*/
+	bzz_thread_t *temp, *tmp;
+	struct list_head *pos, *loop;
+	struct wait_queue_t wait;
+
+	/*iterate through lock's thread list, find the calling thread's entry*/
+
+	list_for_each(pos, &lock->known_threads.list){
+		temp = list_entry(pos,&lock->known_threads.list, list);
+
+		if (temp->tid == current->tgid){
+			break;
+		}
+	}
+
+	if(lock->num_active == 0){ /*all slots are taken*/
+		init_waitqueue_entry(&wait, current);
+		wait.flags |= WQ_FLAG_EXCLUSIVE;
+		wait.func = wait.private->tgid == current->tgid;
+
+		if(temp ->BZZ_COLOR == BZZ_BLACK){
+			add_wait_queue(&lock->black_queue, &wait);
+			lock->black_num++;
+		}
+		else{
+			wait_event_timeout(&lock->gold_queue, wait.func, lock -> timeout);
+			lock->gold_num++;
+		}
+
+		do{
+			/*move timedout gold threads to old_gold_queue*/
+			/*had issues with using the wait queue properly*/
+
+			spin_lock_irqsave(&lock->mutex, lock->flags);
+			if(!(lock->num_active == 0)){
+				lock ->num_active--;
+				spin_lock_irqrestore(&lock->mutex, lock->flags);
+
+				if(temp -> BZZ_COLOR == BZZ_GOLD){
+					if(is_old(temp, lock)){
+						/*run old_gold*/
+					}
+					else if(){/*no black threads are waiting*/
+						/*run gold*/
+					}
+
+				}
+				else{ /*color is black*/
+					/*run black*/
+				}
+			}
+			spin_lock_irqrestore(&lock->mutex, lock->flags);
+		}while(1);
 
 	}
-	else{ /*active thread slots*/
+	else{ /*open thread slots*/
+
+		spin_lock_irqsave(&lock->mutex, lock->flags);
+		lock ->num_active--;
+		spin_lock_irqrestore(&lock->mutex, lock->flags);
 
 	}
-//	if (lock -> num_locked == 0){
-//		lock -> locked = 1;
-//		lock -> locking_thread_PID = gettid();
-//	}
 
-	spin_lock_irqrestore(&lock->mutex, lock->flags);
+
 
 }
 /* Release the lock */
 
 bzz_release (bzz_t *lock){
 	spin_lock_irqsave(&lock->mutex, lock->flags);
-
-//	if (lock -> locked == 1){
-//		if(lock -> locking_thread_PID == gettid()){
-//			lock -> locked = 0;
-//		}
-//	}
+	lock->num_active++;
 	spin_lock_irqrestore(&lock->mutex, lock->flags);
+	wake_up();
 }
 
 /* Destroy the lock */
@@ -82,3 +140,6 @@ bzz_kill(bzz_t lock){
 
 }
 
+int is_old(bzz_thread_t *thread, bzz_t *lock){
+	return (lock->timeout) < ((current_kernel_time().tv_nsec * 1000) - thread -> queue_time );
+}
--- buzzLockK.h
+++ buzzLockK.h
@@ -1,29 +1,39 @@
 #include <linux/types.h>
 #include <linux/spinlock.h>
-#include <list.h>
+#include <semaphore.h>
+#include <linux/list.h>
+#include <linux/time.h>
+#include <linux/wait.h>
 
 #ifndef BUZZLOCKK_H_
 #define BUZZLOCKK_H_
 
-typedef BZZ_BLACK = 1;
-typedef BZZ_GOLD = 0;
+#define BZZ_BLACK 1
+#define BZZ_GOLD 0
 
 typedef struct {
 	struct list_head list;
 	tid_t tid;
 	int BZZ_COLOR;
-	/*time stamp*/
+	long queue_time; /*IN MICROSECONDS*/
 }bzz_thread_t;
 
 typedef struct {
 	spinlock_t mutex = SPIN_LOCK_UNLOCKED;
 	unsigned long flags;
-	int num_locked;
+	int num_active;
 	tid_t locking_thread_tid;
 	suseconds_t timeout;
-	bzz_thread_t known_threads;
-	/*wait_threads queue(s)?*/
+	bzz_thread_t known_threads; /*never delete an entry, only add to tail*/
+	struct wait_queue_head_t gold_queue;
+	struct wait_queue_head_t black_queue;
+	struct wait_queue_head_t old_gold_queue;
+	int black_num;
+	int gold_num;
+	int old_gold_num;
 }bzz_t;
+
+
 
 #include <buzz.h>
 
